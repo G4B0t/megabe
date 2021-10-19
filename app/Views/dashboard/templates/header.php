0ece364fd5b38398129205357408150047dbdba0
Gabriel Efraín Torrejón Sejas <gabrielefrain23@gmail.com>
2021-10-19 19:10:20 -0400

    Prueba Completa 4.5

--- app/Views/dashboard/templates/header.php
+++ app/Views/dashboard/templates/header.php
@@ -20,7 +20,7 @@
   <link href="<?= base_url()?>/vendor/aos/aos.css" rel="stylesheet">
   <link href="<?= base_url()?>/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
   <link href="<?= base_url()?>/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-  <link href="<?= base_url()?>/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <!--<link href="<?=// base_url()?>/vendor/boxicons/css/boxicons.min.css" rel="stylesheet"> -->
   <link href="<?= base_url()?>/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
   <link href="<?= base_url()?>/vendor/remixicon/remixicon.css" rel="stylesheet">
   <link href="<?= base_url()?>/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
